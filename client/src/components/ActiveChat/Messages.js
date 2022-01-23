13e84613eabc05f3f361a73b7ce225c32815805d
Germaine P <me@germainep.com>
2022-01-22 20:39:28 -0500

    feature: Unread message notification
    Added otherUser avatar to the last message the other user has read

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,16 +6,23 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const readMessages = messages.filter(
+    (message) => message.senderId === userId && message.read
+  );
+
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, index, array) => {
         const time = moment(message.createdAt).format("h:mm");
-
         return message.senderId === userId ? (
           <SenderBubble
             key={message.id}
             text={message.text}
             time={time}
+            lastRead={
+              message.read &&
+              readMessages[readMessages.length - 1].id === message.id
+            }
             photo={otherUser.photoUrl}
           />
         ) : (
