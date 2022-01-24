ac7f5ada5ce90f3e4ca7965e990a5dc0a0b420a4
Germaine P <me@germainep.com>
2022-01-24 16:16:02 -0500

    feature: Unread message notification
    made a readMessages constant to hold the read messages making it easier to access the message that was last read

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,14 +6,13 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
-  const readMessages = messages.filter(
-    (message) => message.senderId === userId && message.read
-  );
-
   return (
     <Box>
       {messages.map((message, index, array) => {
         const time = moment(message.createdAt).format("h:mm");
+        const readMessages = messages.filter(
+          (message) => message.senderId === userId && message.read
+        );
         return message.senderId === userId ? (
           <SenderBubble
             key={message.id}
